642d52bd6713fb48903c6edf0588bf71e3c0f54b
ktecheducation <karaulitecheducation@gmail.com>
2025-06-26 12:35:39 +0530

    first commit

---
+++ src/User.jsx
@@ -0,0 +1,13 @@
+function User(props){
+
+  return(
+  <>
+    <p>user name :{ props.name}</p>
+    <p>user name :{ props.course}</p>
+    <p>user name :{ props.age}</p>
+
+  </>
+  )
+}
+
+export {User};
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {User} from "./User";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+<>
+<User name="diksha" course="webdev" age="18"/>
+
+<User name="raksha" course="bca" age="19"/>
+
+<User name="tanvi" course="btech" age="19"/>
+
+</>
+);
+
